1b2095f08231dbb4289c16e3889331b63b18ac32
Your Name <you@example.com>
2022-07-18 05:56:05 +0200

    =first commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+
+Auth::routes(['verify'=>true]);
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
+Route::get('/', function (){
+    return 'Home';
+});
